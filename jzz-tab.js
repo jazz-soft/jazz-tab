92e8767d1156994a988e3dc7c51a94c3f6c0c62c
Sema <jazz-dev@jazz-soft.net>
2024-07-08 08:34:59 -0400

    Added code

--- jzz-tab.js
+++ jzz-tab.js
@@ -1,2 +1,31 @@
+function Parser(s, opt) {
+  this.txt = s;
+  this.prev = 0;
+  this.cur = 0;
+  this.tok = [];
+  while (this.cur < this.txt.length) {
+    var c = this.txt[this.cur];
+    if (/\s/.test(c)) {
+      this.cut();
+      this.prev++;
+    }
+    else if (c == '|') {
+      this.cut();
+      this.cur++;
+      this.cut();
+    }
+    this.cur++;
+  }
+  this.cut();
+}
+
+Parser.prototype.cut = function() {
+  if (this.prev != this.cur) {
+    this.tok.push({ from: this.prev, to: this.cur, txt: this.txt.substring(this.prev, this.cur) });
+    this.prev = this.cur;
+  }
+}
+
 module.exports = {
+  Parser: Parser
 };
